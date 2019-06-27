6d71f3dca245c15f5980dc09201ae69eaaffe907
ilhamsj <ilhamsaputrajati@gmail.com>
2019-06-27 10:50:51 +0700

    pegawai

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,7 +19,7 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->enum('role', ['user', 'admin_bagian', 'admin_kepegawaian']);
+            $table->enum('role', ['pegawai', 'admin_bagian', 'admin_kepegawaian']);
             $table->rememberToken();
             $table->timestamps();
         });
