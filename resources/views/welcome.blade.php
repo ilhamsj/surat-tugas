ca464bbd420f8f52be958d3e73cc42f51bc4faf2
ilhamsj <ilhamsaputrajati@gmail.com>
2019-06-29 19:25:27 +0700

    laravel mix

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,14 +1,35 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8 text-center">
-            <a href="{{route('home')}}" class="btn btn-primary">Home</a>
-            <a href="{{route('pegawai')}}" class="btn btn-primary">Pegawai</a>
-            <a href="{{route('admin')}}" class="btn btn-primary">Admin Bagian</a>
-            <a href="{{route('admin_kepegawaian.index')}}" class="btn btn-primary">Admin Kepegawaian</a>
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/custom.css') }}" rel="stylesheet">
+</head>
+<body>
+    <div id="app" class="height-m">
+        <div class="row">
+            <div class="col-sm-2 bg-dark">Menu</div>
+            <div class="col">
+                <header>Header</header>
+                <main>Content</main>
+                <footer>footer</footer>
+            </div>
         </div>
     </div>
-</div>
-@endsection
+</body>
+</html>
