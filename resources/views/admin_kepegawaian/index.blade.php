e7af153079bbd031d17c6b7f2905b8e032ff935f
ilhamsj <ilhamsaputrajati@gmail.com>
2019-07-01 13:32:46 +0700

    cls

--- resources/views/admin_kepegawaian/index.blade.php
+++ resources/views/admin_kepegawaian/index.blade.php
@@ -1,7 +1,6 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
     <div class="row justify-content-center">
         <div class="col">   
             <table class="table">
@@ -44,5 +43,4 @@
             {{ $pegawai->links() }}
         </div>
     </div>
-</div>
 @endsection
