76dadf979fc74389dcead6f2ae3298800b147033
ilhamsj <ilhamsaputrajati@gmail.com>
2019-09-09 06:53:19 +0700

    lapor dokumentasi fix

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,10 +3,8 @@
 @section('content')
     <div class="container">
         <div class="row">
-            <div class="col-md-12 mb-4" id="capture">
-                @include('include/dashboard_pegawai/index')
-            </div>
             <div class="col-md">
+                @include('include/dashboard_pegawai/index')
                 @include('include/dashboard_pegawai/show')
             </div>
         </div>
