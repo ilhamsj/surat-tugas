bafae8e2ed828fc6c3867670845054a6c3d2f496
ilhamsj <ilhamsaputrajati@gmail.com>
2019-09-09 16:23:31 +0700

    nullable

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,15 +15,15 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('nip');
             $table->string('name');
-            $table->string('jabatan');
-            $table->enum('role', ['admin_bagian', 'admin_kepegawaian', 'pegawai']);
+            $table->string('password');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
             $table->rememberToken();
             $table->timestamps();
+            $table->string('nip')->nullable();;
+            $table->string('jabatan')->nullable();
+            $table->enum('role', ['admin_bagian', 'admin_kepegawaian', 'pegawai']);
         });
     }
 
